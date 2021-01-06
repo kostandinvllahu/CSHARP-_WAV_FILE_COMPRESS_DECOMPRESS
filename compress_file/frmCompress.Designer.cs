04bdaf4694fad7859caeca70d2a9f3fa279adbb7
Kostandin Vllahu <33177557+kostandinvllahu@users.noreply.github.com>
2021-01-06 08:56:44 +0100

    Add files via upload

---
+++ compress_file/frmCompress.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace compress_file
+{
+    partial class frmCompressFile
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboType = new System.Windows.Forms.ComboBox();
+            this.lblType = new System.Windows.Forms.Label();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.btnAccion = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cboType
+            // 
+            this.cboType.FormattingEnabled = true;
+            this.cboType.Items.AddRange(new object[] {
+            "Compress",
+            "Decompress"});
+            this.cboType.Location = new System.Drawing.Point(135, 66);
+            this.cboType.Name = "cboType";
+            this.cboType.Size = new System.Drawing.Size(157, 24);
+            this.cboType.TabIndex = 0;
+            // 
+            // lblType
+            // 
+            this.lblType.AutoSize = true;
+            this.lblType.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblType.Location = new System.Drawing.Point(52, 66);
+            this.lblType.Name = "lblType";
+            this.lblType.Size = new System.Drawing.Size(45, 20);
+            this.lblType.TabIndex = 1;
+            this.lblType.Text = "Type";
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(628, 129);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 4;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // txtFile
+            // 
+            this.txtFile.Location = new System.Drawing.Point(56, 130);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.Size = new System.Drawing.Size(566, 22);
+            this.txtFile.TabIndex = 5;
+            // 
+            // btnAccion
+            // 
+            this.btnAccion.Location = new System.Drawing.Point(56, 178);
+            this.btnAccion.Name = "btnAccion";
+            this.btnAccion.Size = new System.Drawing.Size(75, 23);
+            this.btnAccion.TabIndex = 6;
+            this.btnAccion.Text = "OK";
+            this.btnAccion.UseVisualStyleBackColor = true;
+            this.btnAccion.Click += new System.EventHandler(this.btnAccion_Click);
+            // 
+            // frmCompressFile
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(764, 265);
+            this.Controls.Add(this.btnAccion);
+            this.Controls.Add(this.txtFile);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.lblType);
+            this.Controls.Add(this.cboType);
+            this.Name = "frmCompressFile";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Compress File";
+            this.Load += new System.EventHandler(this.frmCompressFile_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboType;
+        private System.Windows.Forms.Label lblType;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox txtFile;
+        private System.Windows.Forms.Button btnAccion;
+    }
+}
+
---
+++ compress_file/frmCompress.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Windows.Forms;
+
+namespace compress_file
+{
+    public partial class frmCompressFile : Form
+    {
+        public frmCompressFile()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                if(openFileDialog.ShowDialog()==DialogResult.OK)
+                {
+                    txtFile.Text = openFileDialog.FileName;
+                }
+            }
+            catch 
+            {
+
+                MessageBox.Show("An error has occurred", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnAccion_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (cboType.SelectedIndex > -1)
+                {
+                    if (!string.IsNullOrEmpty(txtFile.Text.Trim()))
+                    {
+                        if (File.Exists(txtFile.Text.Trim()))
+                        { 
+                            Cursor.Current = Cursors.WaitCursor;
+                            if (cboType.SelectedIndex == 1)
+                            {
+                                MemoryStream files = new MemoryStream();
+                                using (FileStream file = new FileStream(txtFile.Text, FileMode.Open, FileAccess.Read))
+                                {
+                                    byte[] bytes = new byte[file.Length];
+                                    file.Read(bytes, 0, (int)file.Length);
+                                    files.Write(bytes, 0, (int)file.Length);
+                                }
+                                files.Position = 0;
+
+                                using (var archive = new ZipArchive(files, ZipArchiveMode.Read, true))
+                                {
+                                    for (int i = 0; i <= archive.Entries.Count - 1; i++)
+                                    {
+                                        using (Stream streamZip = archive.Entries[i].Open())
+                                        {
+                                            using (MemoryStream streamFile = new MemoryStream())
+                                            {
+                                                string path = Path.GetDirectoryName(txtFile.Text);
+
+                                                FileStream file = new FileStream($@"{path}\{archive.Entries[i].FullName}", FileMode.Create, FileAccess.Write);
+                                                streamFile.WriteTo(file);
+                                                file.Close();
+                                            }
+                                        }
+                                    }
+                                }
+
+                                MessageBox.Show("decompress finished", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            }
+                            else
+                            {
+                                var zipFile = $@"{Path.GetDirectoryName(txtFile.Text)}\{Path.GetFileNameWithoutExtension(txtFile.Text)}.zip";
+
+                                if(File.Exists(zipFile))
+                                { File.Delete(zipFile); }
+
+                                using (var archive = ZipFile.Open(zipFile, ZipArchiveMode.Create))
+                                {
+                                    archive.CreateEntryFromFile(txtFile.Text, Path.GetFileName(txtFile.Text));
+                                }
+
+                                MessageBox.Show("Compress finished", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("File not exits", "Valitadion", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Please select a file", "Valitadion", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please select a Type", "Valitadion", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            catch 
+            {
+
+                MessageBox.Show("An error has occurred", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                Cursor.Current = Cursors.Default;
+            }
+        }
+
+        private void frmCompressFile_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
